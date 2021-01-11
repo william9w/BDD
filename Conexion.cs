5110c72e14e98f6101cdc76ef50cad3eed63918a
william9w <max.mendoza201@tectijuana.edu.mx>
2021-01-11 11:48:49 -0800

    conexion

---
+++ Conexion.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conexion
+{
+    class Conexion
+    {
+        public static MySqlConnection conexion()
+        {
+            string servidor = "localhost";
+            string bd = "";
+            string usuario = "root";
+            string password = "";
+
+            string cadenaConexion = "Database=" + bd + "; Data Source=" + servidor + "; User Id= " + usuario + "; Password=" + password+"";
+
+            try
+            {
+                MySqlConnection conexionBD = new MySqlConnection(cadenaConexion);
+
+                return conexionBD;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                return null;
+            }
+        }
+    }
+}
